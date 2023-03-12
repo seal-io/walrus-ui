243bf99815c783c751b318e3ba403ed863ccac4e
jialin <jialinkuang@126.com>
2023-03-12 19:08:36 +0800

    chore: terminal api

---
+++ src/components/x-terminal/index.vue
@@ -0,0 +1,270 @@
+<template>
+  <div ref="terminal"></div>
+</template>
+
+<script lang="ts" setup>
+  import qs from 'query-string';
+  import { ref, onMounted, onBeforeUnmount, watch } from 'vue';
+  import { debounce, trim, split, get } from 'lodash';
+  import { Terminal } from 'xterm';
+  import { FitAddon } from 'xterm-addon-fit';
+  import 'xterm/css/xterm.css';
+
+  const props = defineProps({
+    terminalDetail: Object,
+    type: String,
+    url: {
+      type: String,
+      default() {
+        return '';
+      }
+    }
+  });
+  const fitAddon = new FitAddon();
+
+  const terminal = ref(null);
+  const first = ref(true);
+  const loading = ref(true);
+  const terminalSocket = ref<any>(null);
+  const term = ref<any>(null);
+  const command = ref('');
+
+  const runRealTerminal = () => {
+    loading.value = false;
+  };
+  const initData = () => {
+    return `\x1B[1;3;31m\x1B[0m`;
+  };
+  const setData = (data) => {
+    return `- ${data}\x1B[1;3;31m\x1B[0m`;
+  };
+  const setErrorData = (data) => {
+    return `- \x1b[31m${data}\x1b[m`;
+  };
+  // 是否连接中0 1 2 3
+  const isWsOpen = () => {
+    const readyState = terminalSocket.value && terminalSocket.value.readyState;
+    return readyState === 1;
+  };
+  // 发送给后端,调整后端终端大小,和前端保持一致,不然前端只是范围变大了,命令还是会换行
+  const resizeRemoteTerminal = () => {
+    const { cols, rows } = term.value;
+    console.log('wss: resize');
+    if (isWsOpen()) {
+      terminalSocket.value.send(
+        JSON.stringify({
+          Op: 'resize',
+          Cols: cols,
+          Rows: rows
+        })
+      );
+    }
+  };
+  const prompt = () => {
+    command.value = '';
+    term.value.write('\r\n$ ');
+  };
+  const enterPrompt = () => {
+    command.value = '';
+    term.value.write('\r\n');
+  };
+  const runCommand = () => {
+    const data = trim(command.value);
+
+    if (isWsOpen()) {
+      terminalSocket.value.send(
+        JSON.stringify({
+          Op: 'stdin',
+          Data: data
+        })
+      );
+    }
+    enterPrompt();
+    console.log('wss: commond', data, command.value);
+  };
+  const onDataCallback = (e) => {
+    switch (e) {
+      case '\u0003': // Ctrl+C
+        term.value.write('^C');
+        prompt();
+        break;
+      case '\r': // Enter
+        runCommand();
+        break;
+      case '\u007F': // Backspace (DEL)
+        // Do not delete the prompt
+        if (term.value._core.buffer.x > 2) {
+          term.value.write('\b \b');
+          if (command.value.length > 0) {
+            command.value = command.value.substr(0, command.value.length - 1);
+          }
+        }
+        break;
+      default: // Print all other characters
+        if (
+          (e >= String.fromCharCode(0x20) && e <= String.fromCharCode(0x7e)) ||
+          e >= '\u00a0'
+        ) {
+          command.value += e;
+          term.value.write(e);
+        }
+    }
+  };
+  const onWSReceive = (message) => {
+    // 首次接收消息,发送给后端，进行同步适配
+    if (first.value === true) {
+      first.value = false;
+      resizeRemoteTerminal();
+    }
+    const data = JSON.parse(message.data) || '';
+    if (term.value.element) term.value.focus();
+    term.value.write(setData(data.Data));
+    prompt();
+    console.log('wss: receive', message);
+  };
+
+  const errorRealTerminal = (ex) => {
+    let { message } = ex;
+    if (!message) message = 'disconnected';
+    term.value.write(setErrorData(message));
+    prompt();
+    console.log('wss: err', message);
+  };
+  const closeRealTerminal = (data) => {
+    term.value.write(setData(data.reason));
+    prompt();
+    console.log('wss: close:');
+  };
+
+  const createWS = () => {
+    if (!props.url) return;
+    terminalSocket.value = new WebSocket(props.url);
+    terminalSocket.value.onopen = runRealTerminal;
+    terminalSocket.value.onmessage = onWSReceive;
+    terminalSocket.value.onclose = closeRealTerminal;
+    terminalSocket.value.onerror = errorRealTerminal;
+  };
+  const initWS = () => {
+    if (!terminalSocket.value) {
+      createWS();
+    }
+    if (terminalSocket.value && terminalSocket.value.readyState > 1) {
+      terminalSocket.value.close();
+      createWS();
+    }
+  };
+
+  const initTerm = () => {
+    term.value = new Terminal({
+      lineHeight: 1.2,
+      fontSize: 12,
+      fontFamily: "Monaco, Menlo, Consolas, 'Courier New', monospace",
+      theme: {
+        background: '#181d28'
+      },
+      // 光标闪烁
+      cursorBlink: true,
+      cursorStyle: 'underline',
+      scrollback: 100,
+      tabStopWidth: 4
+    });
+    term.value.open(terminal.value);
+    term.value.loadAddon(fitAddon);
+    // term.value.write(initData());
+    // 不能初始化的时候fit,需要等terminal准备就绪,可以设置延时操作
+    setTimeout(() => {
+      fitAddon.fit();
+    }, 100);
+  };
+
+  const fitTerm = () => {
+    fitAddon.fit();
+  };
+  const onResize = debounce(() => fitTerm(), 800);
+
+  const termData = () => {
+    // 输入与粘贴的情况,onData不能重复绑定,不然会发送多次
+    term.value.onData((data) => {
+      console.log('wss: data', data, isWsOpen());
+      onDataCallback(data);
+      // if (isWsOpen()) {
+      //   terminalSocket.value.send(
+      //     JSON.stringify({
+      //       Op: 'stdin',
+      //       Data: data
+      //     })
+      //   );
+      // }
+    });
+  };
+  const onTerminalResize = () => {
+    window.addEventListener('resize', onResize);
+  };
+  const removeResizeListener = () => {
+    window.removeEventListener('resize', onResize);
+  };
+  const setTerminalStatus = () => {
+    if (!terminalSocket?.value) return;
+    const status = terminalSocket?.value?.readyState;
+    if (status === 0) {
+      term.value?.write?.('Connecting...');
+      term.value?.write?.('\r\n$ ');
+    }
+    if (status === 1) {
+      term.value?.write?.(initData());
+    }
+    if (status === 2) {
+      term.value?.write?.('Closing...');
+    }
+    if (status === 3) {
+      term.value.write('Closed!');
+    }
+  };
+  const init = () => {
+    initWS();
+    initTerm();
+    termData();
+    onTerminalResize();
+  };
+  // 监听类型变化，重置term
+  watch(
+    () => props.url,
+    () => {
+      if (!props.url) return;
+      console.log('wss: url===', props.url);
+      first.value = true;
+      loading.value = true;
+      terminalSocket.value = null;
+      initWS();
+      // 重置
+      term.value.reset();
+    },
+    {
+      immediate: true
+    }
+  );
+  watch(
+    () => terminalSocket.value?.readyState,
+    () => {
+      setTerminalStatus();
+    },
+    {
+      immediate: true
+    }
+  );
+  onMounted(() => {
+    init();
+  });
+  onBeforeUnmount(() => {
+    removeResizeListener();
+    console.log('wss: dispose');
+    if (terminalSocket.value) terminalSocket.value.close();
+  });
+</script>
+
+<style lang="less" scoped>
+  #terminal {
+    width: 100%;
+    height: 100%;
+  }
+</style>
--- src/views/application-management/applications/components/instance/x-terminal/tab-terminal.vue
+++ src/views/application-management/applications/components/instance/x-terminal/tab-terminal.vue
@@ -1,166 +1,60 @@
 <template>
-  <div ref="terminal"></div>
+  <div>
+    <a-cascader
+      :model-value="resourceKey"
+      style="width: 240px; margin-bottom: 8px"
+      :options="containerList"
+      placeholder="请选择对象"
+      @change="handleObjectChange"
+    ></a-cascader>
+    <xTerminal :url="wssURL"></xTerminal>
+  </div>
 </template>
 
 <script lang="ts" setup>
-  import { ref, onMounted, onBeforeUnmount, watch } from 'vue';
-  import { debounce } from 'lodash';
-  import { Terminal } from 'xterm';
-  import { FitAddon } from 'xterm-addon-fit';
-  import 'xterm/css/xterm.css';
+  import { inject, ref, computed, onMounted } from 'vue';
+  import xTerminal from '@/components/x-terminal/index.vue';
+  import { queryApplicationResource } from '../../../api';
+  import { Cascader } from '../../../config/interface';
+  import { generateResourcesKeys, getResourceId } from '../../../config';
 
-  const props = defineProps({
-    terminalDetail: Object,
-    type: String,
-    url: {
-      type: String,
-      default() {
-        return '';
-      }
+  const localServer = window.location.host;
+  const instanceId = inject('instanceId', ref(''));
+  const resourceId = ref('');
+  const resourceKey = ref('');
+  const containerList = ref<Cascader[]>([]);
+
+  const wssURL = computed(() => {
+    if (!resourceId.value || !resourceKey.value) {
+      return '';
     }
+    return `wss://${localServer}/v1/application-resources/${resourceId.value}/exec?key=${resourceKey.value}`;
   });
-  const fitAddon = new FitAddon();
-
-  const terminal = ref(null);
-  const first = ref(true);
-  const loading = ref(true);
-  const terminalSocket = ref<any>(null);
-  const term = ref<any>(null);
-
-  const runRealTerminal = () => {
-    loading.value = false;
+  const handleObjectChange = (val) => {
+    const result = getResourceId(val);
+    resourceKey.value = result.key;
+    resourceId.value = result.id;
   };
-  // 是否连接中0 1 2 3
-  const isWsOpen = () => {
-    const readyState = terminalSocket.value && terminalSocket.value.readyState;
-    return readyState === 1;
-  };
-  // 发送给后端,调整后端终端大小,和前端保持一致,不然前端只是范围变大了,命令还是会换行
-  const resizeRemoteTerminal = () => {
-    const { cols, rows } = term.value;
-    if (isWsOpen()) {
-      terminalSocket.value.send(
-        JSON.stringify({
-          Op: 'resize',
-          Cols: cols,
-          Rows: rows
-        })
-      );
+  const getApplicationResource = async () => {
+    if (!instanceId.value) return;
+    try {
+      const params = {
+        instanceID: instanceId.value,
+        page: 1,
+        perPage: -1
+      };
+      const { data } = await queryApplicationResource(params);
+      const list = data?.items || [];
+      // const list = testData;
+      containerList.value = generateResourcesKeys(list, 'executable');
+    } catch (error) {
+      console.log(error);
+      containerList.value = [];
     }
   };
-  const onWSReceive = (message) => {
-    // 首次接收消息,发送给后端，进行同步适配
-    if (first.value === true) {
-      first.value = false;
-      resizeRemoteTerminal();
-    }
-    const data = JSON.parse(message.data);
-    if (term.value.element) term.value.focus();
-    term.value.write(data.Data);
-  };
-
-  const errorRealTerminal = (ex) => {
-    let { message } = ex;
-    if (!message) message = 'disconnected';
-    term.value.write(`\x1b[31m${message}\x1b[m\r\n`);
-    console.log('err');
-  };
-  const closeRealTerminal = () => {
-    console.log('close');
-  };
-
-  const createWS = () => {
-    if (!props.url) return;
-    terminalSocket.value = new WebSocket(props.url);
-    terminalSocket.value.onopen = runRealTerminal;
-    terminalSocket.value.onmessage = onWSReceive;
-    terminalSocket.value.onclose = closeRealTerminal;
-    terminalSocket.value.onerror = errorRealTerminal;
-  };
-  const initWS = () => {
-    if (!terminalSocket.value) {
-      createWS();
-    }
-    if (terminalSocket.value && terminalSocket.value.readyState > 1) {
-      terminalSocket.value.close();
-      createWS();
-    }
-  };
-
-  const initTerm = () => {
-    term.value = new Terminal({
-      lineHeight: 1.2,
-      fontSize: 12,
-      fontFamily: "Monaco, Menlo, Consolas, 'Courier New', monospace",
-      theme: {
-        background: '#181d28'
-      },
-      // 光标闪烁
-      cursorBlink: true,
-      cursorStyle: 'underline',
-      scrollback: 100,
-      tabStopWidth: 4
-    });
-    term.value.open(terminal.value);
-    term.value.loadAddon(fitAddon);
-    // 不能初始化的时候fit,需要等terminal准备就绪,可以设置延时操作
-    setTimeout(() => {
-      fitAddon.fit();
-    }, 5);
-  };
-
-  const fitTerm = () => {
-    fitAddon.fit();
-  };
-  const onResize = debounce(() => fitTerm(), 800);
-
-  const termData = () => {
-    // 输入与粘贴的情况,onData不能重复绑定,不然会发送多次
-    term.value.onData((data) => {
-      if (isWsOpen()) {
-        terminalSocket.value.send(
-          JSON.stringify({
-            Op: 'stdin',
-            Data: data
-          })
-        );
-      }
-    });
-  };
-  const onTerminalResize = () => {
-    window.addEventListener('resize', onResize);
-  };
-  const removeResizeListener = () => {
-    window.removeEventListener('resize', onResize);
-  };
-  // 监听类型变化，重置term
-  watch(
-    () => props.type,
-    () => {
-      first.value = true;
-      loading.value = true;
-      terminalSocket.value = null;
-      initWS();
-      // 重置
-      term.value.reset();
-    }
-  );
-
   onMounted(() => {
-    initWS();
-    initTerm();
-    termData();
-    onTerminalResize();
-  });
-  onBeforeUnmount(() => {
-    removeResizeListener();
-    if (terminalSocket.value) terminalSocket.value.close();
+    getApplicationResource();
   });
 </script>
 
-<style lang="less" scoped>
-  #terminal {
-    width: 100%;
-    height: 100%;
-  }
-</style>
+<style lang="less" scoped></style>
