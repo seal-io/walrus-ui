afeff4ac2add5aea3dbb315633d357b0123443b9
jialin <jialinkuang@126.com>
2023-03-31 10:43:38 +0800

    style: dashborad status

--- src/views/dashboard/config/index.ts
+++ src/views/dashboard/config/index.ts
@@ -3,11 +3,18 @@ import { riskColorMap } from '@/config/global';
 import dayjs from 'dayjs';
 
 export const statusColorMap = {
-  running: 'rgba(87, 180, 230, 1)',
+  running: 'rgba(247, 186, 30,1)',
   auditing: 'rgba(135, 145, 247, .5)',
-  failed: 'rgba(242, 166, 159, 1)',
+  failed: 'rgba(245, 63, 63,.7)',
   succeed: 'rgba(128, 212, 197, 1)',
   succeeded: 'rgba(128, 212, 197, 1)'
+};
+export const chartStatusColorMap = {
+  running: 'rgba(247, 186, 30,.5)',
+  auditing: 'rgba(135, 145, 247, .5)',
+  failed: 'rgba(245, 63, 63,.5)',
+  succeed: 'rgba(128, 212, 197, .8)',
+  succeeded: 'rgba(128, 212, 197, .8)'
 };
 
 export const deployDataConfig = [
@@ -16,21 +23,21 @@ export const deployDataConfig = [
     value: 0,
     key: 'succeed',
     label: { show: false },
-    itemStyle: { color: statusColorMap.succeed }
+    itemStyle: { color: chartStatusColorMap.succeed }
   },
   {
     name: 'dashboard.deployment.failed',
     value: 0,
     key: 'failed',
     label: { show: false },
-    itemStyle: { color: statusColorMap.failed }
+    itemStyle: { color: chartStatusColorMap.failed }
   },
   {
     name: 'dashboard.deployment.running',
     value: 0,
     key: 'running',
     label: { show: false },
-    itemStyle: { color: statusColorMap.running }
+    itemStyle: { color: chartStatusColorMap.running }
   }
   // {
   //   name: '审核中',
