df4ea8d6668cc91ba2efec6cb02de7da5e52bdd1
jialin <jialinkuang@126.com>
2023-05-11 17:19:11 +0800

    chore: tab graph

---
+++ src/views/application-management/applications/components/instance/tab-graph/components/node-tool-bar.vue
@@ -0,0 +1,6 @@
+<template> <div>ds</div></template>
+
+<script lang="ts" setup>
+  import { Handle, Position } from '@vue-flow/core';
+  import { NodeToolbar } from '@vue-flow/node-toolbar';
+</script>
