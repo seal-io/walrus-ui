7ab3690924b7b83c8fad047fec0f80af3be21038
jialin <jialinkuang@126.com>
2023-06-25 14:37:49 +0800

    feat: api tokens create

--- config/vite.config.dev.ts
+++ config/vite.config.dev.ts
@@ -18,7 +18,7 @@ export default mergeConfig(
       },
       https: true,
       port: 4000,
-      proxy: createProxyTable('https://192.168.50.123')
+      proxy: createProxyTable('https://192.168.50.92')
     },
     plugins: [
       eslint({
