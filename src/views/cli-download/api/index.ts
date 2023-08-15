3433e89d216d693faf6f8a50840e7eb7ea3298a2
jialin <jialinkuang@126.com>
2023-08-15 20:55:05 +0800

    chore: download cli sync

--- src/views/cli-download/api/index.ts
+++ src/views/cli-download/api/index.ts
@@ -1,7 +1,11 @@
 import axios from 'axios';
 import qs from 'query-string';
 
-export const downloadCli = (params: { os: string; arch: string }) => {
+export const downloadCli = (params: {
+  os: string;
+  arch: string;
+  t: number;
+}) => {
   return `/cli?${qs.stringify(params)}`;
 };
 
