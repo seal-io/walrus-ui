4cac8d0055b9471c7ae28d6082441d97be47e799
jialin <jialinkuang@126.com>
2023-09-12 17:43:48 +0800

    fix: user permission table list order

--- src/permissions/config/project.ts
+++ src/permissions/config/project.ts
@@ -1,4 +1,9 @@
-import { Resources, Group, ResourcesOrder } from './index';
+import {
+  Resources,
+  Group,
+  ResourcesOrder,
+  ProjectResourcesOrder
+} from './index';
 
 export default [
   {
@@ -6,6 +11,7 @@ export default [
     title: 'permission.resource.project.appResource',
     group: Group.AppManagement,
     order: ResourcesOrder.ServiceResources,
+    resourceOrder: ProjectResourcesOrder.ServiceResources,
     actions: ['GET']
   },
   {
@@ -13,6 +19,7 @@ export default [
     title: 'permission.resource.project.appRevision',
     group: Group.AppManagement,
     order: ResourcesOrder.ServiceRevisions,
+    resourceOrder: ProjectResourcesOrder.ServiceRevisions,
     actions: ['GET', 'DELETE']
   },
   {
@@ -20,6 +27,7 @@ export default [
     title: 'permission.resource.project.roleAuth',
     group: Group.AppManagement,
     order: ResourcesOrder.ProjectSubjects,
+    resourceOrder: ProjectResourcesOrder.ProjectSubjects,
     actions: ['GET', 'POST', 'DELETE', 'PUT']
   },
   {
@@ -27,6 +35,7 @@ export default [
     title: 'permission.resource.project.secret',
     group: Group.OperatorHub,
     order: ResourcesOrder.Variables,
+    resourceOrder: ProjectResourcesOrder.Variables,
     actions: ['GET', 'POST', 'DELETE', 'PUT']
   },
   {
@@ -34,6 +43,7 @@ export default [
     title: 'permission.resource.project.appList',
     group: Group.AppManagement,
     order: ResourcesOrder.Services,
+    resourceOrder: ProjectResourcesOrder.Services,
     actions: ['GET', 'POST', 'DELETE', 'PUT']
   },
   {
@@ -41,6 +51,23 @@ export default [
     title: 'permission.button.action',
     group: Group.AppManagement,
     order: ResourcesOrder.Projects,
+    resourceOrder: ProjectResourcesOrder.Projects,
+    actions: ['GET', 'POST', 'DELETE', 'PUT']
+  },
+  {
+    resource: Resources.Connectors,
+    title: 'permission.resource.system.operation.connector',
+    group: Group.OperatorHub,
+    order: ResourcesOrder.Connectors,
+    resourceOrder: ProjectResourcesOrder.Connectors,
+    actions: ['GET', 'POST', 'DELETE', 'PUT']
+  },
+  {
+    resource: Resources.Environments,
+    title: 'menu.operatorHub.evniroment',
+    group: Group.AppManagement,
+    order: ResourcesOrder.Environments,
+    resourceOrder: ProjectResourcesOrder.Environments,
     actions: ['GET', 'POST', 'DELETE', 'PUT']
   }
 ];
