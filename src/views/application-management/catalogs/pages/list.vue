4b3fff39474a9cbc6f9ce8013edd977d7ed8a6f4
jialin <jialinkuang@126.com>
2023-10-08 15:23:43 +0800

    feat: project scope templates

---
+++ src/views/application-management/catalogs/pages/list.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <TableList scope="project"></TableList>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import TableList from '@/views/operation-hub/catalogs/components/table-list.vue';
+</script>
---
+++ src/views/application-management/templates/pages/list.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <TableList scope="project" :current-view="currentView"></TableList>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import TableList from '@/views/operation-hub/templates/components/table-list.vue';
+
+  defineProps({
+    currentView: {
+      type: String,
+      default: 'thumb'
+    }
+  });
+</script>
