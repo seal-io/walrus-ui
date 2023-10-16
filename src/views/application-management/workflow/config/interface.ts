0b76f7e6da0b50c1eb759d513b6d3a0c35b72d9b
jialin <jialinkuang@126.com>
2023-10-16 09:38:25 +0800

    chore: api list

--- src/views/application-management/workflow/config/interface.ts
+++ src/views/application-management/workflow/config/interface.ts
@@ -1,14 +1,37 @@
 export interface Step {
+  id?: string;
   dependencies: string[];
   description: string;
   input: object;
   labels: object;
+  status?: Record<string, any>;
   name: string;
   spec: Record<string, any>;
 }
 export interface Stage {
+  id?: string;
   dependencies: string[];
   description: string;
   name: string;
   steps: Step[];
+}
+
+export interface PipelineRow {
+  createTime: string;
+  description: string;
+  displayName: string;
+  environmentID: string;
+  id: string;
+  name: string;
+  executions: Array<Step>;
+  labels: object;
+  project: {
+    id: string;
+    name: string;
+  };
+  stageIds: string[];
+  status: object;
+  type: string;
+  updateTime: string;
+  stages: Stage[];
 }
