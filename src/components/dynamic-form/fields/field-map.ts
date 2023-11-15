210e828543de308a45117e8d44a98950681a8191
jialin <jialinkuang@126.com>
2023-11-15 12:14:44 +0800

    style: add button style

--- src/components/dynamic-form/fields/field-map.ts
+++ src/components/dynamic-form/fields/field-map.ts
@@ -32,95 +32,79 @@ import { FieldSchema } from '../interface';
 import ComponentsMap from '../components/components-map';
 import FIELD_TYPE from '../config/field-type';
 
-export default {
-  Input: HintInput,
-  InputNumber: sealInputNumber,
-  InputPassword: sealInputPassword,
-  Select: sealSelect,
-  Option,
-  Radio,
-  RadioGroup,
-  Checkbox: sealCheckbox,
-  CheckboxGroup,
-  Switch: sealSwitch,
-  HintInput,
-  Textarea: sealTextarea,
-  AceEditor
+export const CommonFieldMaps = {
+  textArea: sealTextarea,
+  select: sealSelect,
+  password: sealInputPassword,
+  aceEditor: AceEditor,
+  xInputGroup,
+  radioGroup: RadioGroup,
+  checkboxGroup: CheckboxGroup
 };
 
 export const FieldMaps = {
   array: ArrayField,
   object: ObjectField,
-  boolean: sealCheckbox,
-  integer: sealInputNumber,
-  number: sealInputNumber,
-  string: stringField
+  stringField
 };
 
-export const FieldTypes = {
-  types: {
-    boolean: sealSwitch,
-    string: HintInput,
-    number: sealInputNumber,
-    integer: sealInputNumber,
-    array: ArrayField,
-    object: ObjectField
-  },
-  formats: {
-    'time': datePicker,
-    'date': datePicker,
-    'date-time': datePicker
-  },
-  common: {
-    textArea: sealTextarea,
-    select: sealSelect,
-    password: sealInputPassword,
-    aceEditor: AceEditor,
-    xInputGroup,
-    radioGroup: RadioGroup,
-    checkboxGroup: CheckboxGroup
-  }
+export const BasicFieldMaps = {
+  boolean: sealCheckbox,
+  string: HintInput,
+  number: sealInputNumber,
+  integer: sealInputNumber
+};
+
+export const FormatsFieldMaps = {
+  'time': datePicker,
+  'date': datePicker,
+  'date-time': datePicker
 };
 
 export const isMuliSelect = (schema: FieldSchema) => {
+  const { type, enum: enumList, items } = schema;
+  return (
+    type === FIELD_TYPE.ARRAY &&
+    (enumList?.length || items?.type === FIELD_TYPE.STRING)
+  );
+};
+
+export const isFixedOptionSelect = (schema: FieldSchema) => {
   const { type, enum: enumList } = schema;
-  return type === FIELD_TYPE.ARRAY && enumList && enumList.length > 0;
+  return type === FIELD_TYPE.STRING && enumList?.length;
+};
+
+export const isAllowCreateSelect = (schema: FieldSchema) => {
+  const { type, enum: enumList } = schema;
+  return type === FIELD_TYPE.STRING && !enumList?.length;
 };
 
 export const getSchemaFieldComponent = ({ schema, fieldPath, formData }) => {
   const { type } = schema;
   const widget = _.get(schema, ['x-walrus-ui', 'widget'], '');
 
+  // build-in component
   if (widget) {
     return {
       component: ComponentsMap[widget],
       fieldPath: [...fieldPath]
     };
   }
-  if (
-    type === FIELD_TYPE.OBJECT &&
-    _.get(schema, 'additionalProperties.type') === FIELD_TYPE.STRING
-  ) {
-    return {
-      component: FieldTypes.common.xInputGroup,
-      fieldPath: [...fieldPath]
-    };
-  }
 
-  if (isMuliSelect(schema)) {
+  if (type === FIELD_TYPE.ARRAY && !isMuliSelect(schema)) {
     return {
-      component: FieldTypes.common.select,
+      component: FieldMaps.array,
       fieldPath: [...fieldPath]
     };
   }
-  if (type) {
+  if (type === FIELD_TYPE.OBJECT) {
     return {
-      component: FieldTypes.types[type],
+      component: FieldMaps.object,
       fieldPath: [...fieldPath]
     };
   }
   return {
-    component: null,
-    fieldPath: []
+    component: FieldMaps.stringField,
+    fieldPath: [...fieldPath]
   };
 };
