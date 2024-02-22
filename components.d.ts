6040f0c0dcd00b577ce6066dfbf356819346d43d
jialin <jialinkuang@126.com>
2024-02-22 19:21:27 +0800

    fix: build error

--- components.d.ts
+++ components.d.ts
@@ -6,6 +6,7 @@ declare module 'vue' {
   export interface GlobalComponents {
     AAffix: typeof import('@arco-design/web-vue')['Affix'];
     AAlert: typeof import('@arco-design/web-vue')['Alert'];
+    AAvatar: typeof import('@arco-design/web-vue')['Avatar'];
     ABreadcrumb: typeof import('@arco-design/web-vue')['Breadcrumb'];
     ABreadcrumbItem: typeof import('@arco-design/web-vue')['BreadcrumbItem'];
     AButton: typeof import('@arco-design/web-vue')['Button'];
