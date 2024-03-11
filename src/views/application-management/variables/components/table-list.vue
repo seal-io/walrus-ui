b87877104f989c7158be0e1fa6c03c253b9abca4
jialin <jialinkuang@126.com>
2024-03-11 18:42:33 +0800

    fix(style): card default transition none

--- src/views/application-management/variables/components/table-list.vue
+++ src/views/application-management/variables/components/table-list.vue
@@ -177,7 +177,11 @@
           >
             <template #icon>
               <icon-find-replace v-if="queryParams.query" />
-              <i v-else class="iconfont icon-variable"></i>
+              <i
+                v-else
+                class="iconfont icon-variable"
+                style="font-weight: 500; font-size: 24px"
+              ></i>
             </template>
             <template #extra>
               <a-button
